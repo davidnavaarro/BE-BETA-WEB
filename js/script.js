273625e0009bf0d415507569ea6d6b62f2282284
davidnavaarro <navarromartinez24@gmail.com>
2025-01-10 12:45:52 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -72,10 +72,7 @@ document.addEventListener('DOMContentLoaded', () => {
 
 // import LocomotiveScroll from 'locomotive-scroll';
 
-// const scroll = new LocomotiveScroll({
-//     el: document.querySelector('[data-scroll-container]'),
-//     smooth: true
-// });
+
 
 
 // CARRUSEL PRODUCTOS
